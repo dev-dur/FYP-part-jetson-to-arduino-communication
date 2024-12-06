a926df608c9acf37c82ca99f5e3c288d15b824d3
DurE Shahwar <59398146+dev-dur@users.noreply.github.com>
2024-12-06 17:29:54 +0500

    Update comm jet-mega.py

--- comm jet-mega.py
+++ comm jet-mega.py
@@ -1,24 +1,23 @@
 import serial
 import time
 
-# Replace '/dev/ttyACM0' with the correct port for your Arduino Mega
-arduino_port = '/dev/ttyACM0'
+
+arduino_port = '/dev/ttyUSB0'
 baud_rate = 9600  # Same baud rate as set in the Arduino code
 
 try:
     arduino = serial.Serial(arduino_port, baud_rate, timeout=1)
-    time.sleep(2)  # Wait for the connection to initialize
-
+    time.sleep(2)  
     print("Connected to Arduino")
 
-    # Send commands to Arduino
+
     while True:
         command = input("Enter command (1 to turn ON, 0 to turn OFF, q to quit): ")
         if command == 'q':
             print("Exiting program...")
             break
         elif command in ['0', '1']:
-            arduino.write(command.encode())  # Send the command to Arduino
+            arduino.write(command.encode())  
             print(f"Command '{command}' sent.")
         else:
             print("Invalid command. Please enter '1', '0', or 'q'.")
